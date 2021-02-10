9b3b2fc9785d87edfc3b4582e70e43f025f237ca
Zoe <zoeoklein@gmail.com>
2021-02-09 17:24:18 -0800

    .

--- privateProtected.py
+++ privateProtected.py
@@ -1,5 +1,5 @@
 
-#protected 
+#private/protected
 class Protected:
     def __init__(self):
         #Private is indicated with the double underscore
@@ -15,3 +15,4 @@ obj = Protected()
 obj.getPrivate()
 obj.setPrivate(35)
 obj.getPrivate()
+
